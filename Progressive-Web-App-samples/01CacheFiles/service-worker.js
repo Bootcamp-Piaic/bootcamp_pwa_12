892f3619c44036a5eaf771a99413e38b0f04b097
Owais Mansha <awaismansha1998@gmail.com>
2020-12-03 20:39:45 +0500

    practice and sample codes

---
+++ Progressive-Web-App-samples/01CacheFiles/service-worker.js
@@ -0,0 +1,32 @@
+
+var cacheName = 'demo-app';
+
+var filesToCache = [
+  './',
+  './app.js',
+  './style.css',
+  './index.html',  
+];
+
+self.addEventListener("activate", function(e) {
+  console.log("[ServiceWorker] Activated");
+});
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+   caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
